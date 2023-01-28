3634e42894e53056a8b1d456edef4f78d4ee9e5e
“abdalrhman772 <abdalrhmanmahmoud27@gmail.com>
2023-01-28 23:09:17 +0200

    lab1.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,16 +10,16 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">ITI Blog Post</a>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: rgb(21, 21, 58)">
+    <div class="container-fluid ">
+        <a class="navbar-brand text-light fw-bold" href="#">ITI Blog Post</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-               <a class="nav-link active" href="{{route('posts.index')}}">All Posts</a>
+            <div class="navbar-brand fw-bold">
+               <a class="nav-link active text-light" href="{{route('posts.index')}}">All Posts</a>
             </div>
         </div>
     </div>
